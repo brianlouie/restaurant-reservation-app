a9adcb2f8cb74da9c20dd42b1d44234b848039a3
Louie Thompson <louieteesmn@gmail.com>
2025-08-28 05:18:56 -0500

    updated all databases

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgres://mdbmkzyc:7RnD8Yqe8ZHisf9OeBuYrkZZ3V9Zx7Kb@chunee.db.elephantsql.com/mdbmkzyc",
-  DATABASE_URL_DEVELOPMENT = "postgres://zhknmyap:6aGOqqWtAt4z26eUuY3NYQIPKkrXGJLO@chunee.db.elephantsql.com/zhknmyap",
-  DATABASE_URL_TEST = "postgres://ghzlnfxs:XyeMQ9nrdJtysDtHRqlaGGI3SatFVp9-@chunee.db.elephantsql.com/ghzlnfxs",
-  DATABASE_URL_PREVIEW = "postgres://gsufpebn:4s-uHzgOb9J4txzS5EPyJG3nbqEY_6PP@chunee.db.elephantsql.com/gsufpebn",
+  DATABASE_URL = "postgresql://postgres:DHRsvnftdwtBzFtk@db.jwzznagnnpyaqluqdwph.supabase.co:5432/postgres",
+  DATABASE_URL_DEVELOPMENT = "postgresql://postgres:DHRsvnftdwtBzFtk@db.jwzznagnnpyaqluqdwph.supabase.co:5432/postgres",
+  DATABASE_URL_TEST = "postgresql://postgres:DHRsvnftdwtBzFtk@db.jwzznagnnpyaqluqdwph.supabase.co:5432/postgres",
+  DATABASE_URL_PREVIEW = "postgresql://postgres:DHRsvnftdwtBzFtk@db.jwzznagnnpyaqluqdwph.supabase.co:5432/postgres",
   DEBUG,
 } = process.env;
 
